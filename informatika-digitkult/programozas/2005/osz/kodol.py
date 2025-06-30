b2777c4918438645204a814f995e4e5860edab64
Durucz Béla <30567460+the-developer-guy@users.noreply.github.com>
2025-06-30 13:00:55 +0200

    2025 osz kesz

--- informatika-digitkult/programozas/2005/osz/kodol.py
+++ informatika-digitkult/programozas/2005/osz/kodol.py
@@ -42,7 +42,67 @@ for karakter in szoveg:
             
         atalakitott_szoveg += nagybetus_karakter
 
+
 # 3. feladat
 # Írja ki a képernyőre az átalakított nyílt szöveget!
 
 print(atalakitott_szoveg)
+
+
+# 4. feladat
+# Kérjen be a felhasználótól egy maximum 5 karakteres, nem üres kulcsszót!
+# A kulcsszó a kódolás feltételeinek megfelelő legyen!
+# (Sem átalakítás, sem ellenőrzés nem kell!)
+# Alakítsa át a kulcsszót csupa nagybetűssé!
+
+bekert_kulcsszo = input("Adjon meg egy legfeljebb 5 karakteres kulcsszót: ")
+kulcsszo = bekert_kulcsszo.upper()
+
+
+# 5. feladat
+# A kódolás első lépéseként fűzze össze a kulcsszót egymás után annyiszor,
+# hogy az így kapott karaktersorozat (továbbiakban kulcsszöveg) hossza
+# legyen egyenlő a kódolandó szöveg hosszával!
+# Írja ki a képernyőre az így kapott kulcsszöveget!
+
+kulcsszoveg = ""
+kulcsszo_hossz = len(kulcsszo)
+for i in range(len(atalakitott_szoveg)):
+    kulcsszoveg += kulcsszo[i % kulcsszo_hossz]
+
+print(f"Kulcsszöveg: {kulcsszoveg}")
+
+
+# 6. feladat
+# A kódolás második lépéseként a következőket hajtsa végre!
+# Vegye az átalakított nyílt szöveg első karakterét,
+# és keresse meg a vtabla.dat fájlból beolvasott táblázat első oszlopában!
+# Ezután vegye a kulcsszöveg első karakterét,
+# és keresse meg a táblázat első sorában!
+# Az így kiválasztott sor és oszlop metszéspontjában lévő karakter lesz
+# a kódolt szöveg első karaktere.
+# Ezt ismételje a kódolandó szöveg többi karakterével is!
+
+kodtabla = []
+with open("Vtabla.dat", "rt", encoding="utf-8") as file:
+    for line in file:
+        kodtabla.append(line)
+
+kodolt_szoveg = ""
+print(f"'A' kódja {ord('A')}")
+for i in range(len(atalakitott_szoveg)):
+    oszlop_karakter = atalakitott_szoveg[i]
+    sor_karakter = kulcsszoveg[i]
+    oszlop_index = ord(oszlop_karakter) - 65
+    sor_index = ord(sor_karakter) - 65
+
+    kodolt_szoveg += kodtabla[sor_index][oszlop_index]
+
+
+# 7. feladat
+# Írja ki a képernyőre és a kodolt.dat fájlba a kapott kódolt szöveget!
+
+print(f"A kódolt szöveg: {kodolt_szoveg}")
+
+with open("kodolt.dat", "wt", encoding="utf-8") as file:
+    file.write(kodolt_szoveg)
